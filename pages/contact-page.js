2b36d0da9e23bd4d4ad905b9b2c0df7f07f08d36
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-04 17:41:07 +0900

    add contact-page text and image

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,9 +1,23 @@
 import Layout from "../components/Layout";
+import Image from "next/image";
 
 function Contact() {
   return (
     <Layout title="Contact">
-      <div>Contact page</div>
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <p className="font-bold">Contact info</p>
+        </div>
+        <div className="flex justify-center mt-4">
+          <Image
+            className="rounded-full"
+            src="/avatar.jpg"
+            width={60}
+            height={60}
+            alt="Avatar"
+          />
+        </div>
+      </div>
     </Layout>
   );
 }
