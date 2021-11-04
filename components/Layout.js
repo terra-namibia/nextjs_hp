28b8576142b0b0fee25e376fcad1d755f9704fcc
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-04 15:54:25 +0900

    add Home in nav

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,17 @@ function Layout({ children, title = "HP by Nextjs" }) {
         <title>{title}</title>
       </Head>
       <header>
-        <nav className="bg-gray-800 w-screen"></nav>
+        <nav className="bg-gray-800 w-screen">
+          <div className="flex items-center pl-8 h-14">
+            <div className="flex space-x-4">
+              <Link href="/">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Home
+                </a>
+              </Link>
+            </div>
+          </div>
+        </nav>
       </header>
       <main className="flex flex-1 justify-center items-center flex-col w-screen">
         {children}
