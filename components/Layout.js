83eaa044ba94ccbf0f441363859bc7b16d31ea0c
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-04 16:35:17 +0900

    add footer

--- components/Layout.js
+++ components/Layout.js
@@ -33,6 +33,16 @@ function Layout({ children, title = "HP by Nextjs" }) {
       <main className="flex flex-1 justify-center items-center flex-col w-screen">
         {children}
       </main>
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <a
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+        </a>
+      </footer>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,5 @@ export default function Home() {
     <Layout title="Home">
       <p className="text-4xl">Hello Next</p>
     </Layout>
-    // <footer className={styles.footer}>
-    //   <a
-    //     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-    //     target="_blank"
-    //     rel="noopener noreferrer"
-    //   >
-    //     Powered by{" "}
-    //     <span className={styles.logo}>
-    //       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    //     </span>
-    //   </a>
-    // </footer>
   );
 }
