77a9070ccce584b58d4db4dc3b68be6c9fce6ec0
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-04 19:37:28 +0900

    add contact-page texts

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -17,6 +17,14 @@ function Contact() {
             alt="Avatar"
           />
         </div>
+        <div className="mt-4">
+          <p className="font-bold">Address</p>
+          <p className="text-xs mt-2 text-gray-600">city A</p>
+          <p className="font-bold mt-3">E-mail</p>
+          <p className="text-xs mt-2 text-gray-600">abc@gmail.com</p>
+          <p className="font-bold mt-3">Phone</p>
+          <p className="text-xs mt-2 text-gray-600">000-123-456</p>
+        </div>
       </div>
     </Layout>
   );
