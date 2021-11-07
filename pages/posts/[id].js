3d6123400d12c99847271855a39bc6df4fee3aba
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-07 10:21:59 +0900

    add svg icon: Back to blog-page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -17,6 +17,18 @@ export default function Post({ post }) {
       <p className="px-10">{post.body}</p>
       <Link href="/blog-page">
         <div className="flex cursor-pointer mt-12">
+          <svg
+            className="w-6 h-6 mr-3"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
           <span>Back to blog-page</span>
         </div>
       </Link>
